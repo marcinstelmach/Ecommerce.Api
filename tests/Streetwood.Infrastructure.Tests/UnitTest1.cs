973eda46bad4fbf5f0c633582f38f56cdb16888d
marcinstelmach <marcinxd4@gmail.com>
2018-09-21 19:00:53 +0200

    Init Commit

---
+++ tests/Streetwood.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Streetwood.Infrastructure.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
