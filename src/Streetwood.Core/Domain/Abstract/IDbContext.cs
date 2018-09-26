e1d52b31323997796c67a6619f8d9f0a8aa20e89
Marcin Stelmach <mstelmach@pgs-soft.com>
2018-09-26 15:09:37 +0200

    Updated DbScript of new Entities and Configurations

--- src/Streetwood.Core/Domain/Abstract/IDbContext.cs
+++ src/Streetwood.Core/Domain/Abstract/IDbContext.cs
@@ -8,29 +8,33 @@ namespace Streetwood.Core.Domain.Abstract
 {
     public interface IDbContext : IDisposable
     {
-        DbSet<User> Users { get; set; }
-
         DbSet<Address> Addresses { get; set; }
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<Charm> Charms { set; get; }
+
+        DbSet<CharmCategory> CharmCategories { get; set; }
+
+        DbSet<Image> Images { get; set; }
 
         DbSet<Order> Orders { get; set; }
 
-        DbSet<Shippment> Shippments { get; set; }
+        DbSet<OrderDiscount> OrderDiscounts { get; set; }
 
-        DbSet<ProductDiscount> Discounts { get; set; }
+        DbSet<Product> Products { get; set; }
+
+        DbSet<ProductCategory> ProductCategories { get; set; }
+
+        DbSet<ProductCategoryDiscount> ProductCategoryDiscounts { get; set; }
 
         DbSet<ProductOrder> ProductOrders { get; set; }
 
         DbSet<ProductOrderCharm> ProductOrderCharms { get; set; }
 
-        DbSet<Image> Images { get; set; }
+        DbSet<Shippment> Shippments { get; set; }
 
-        DbSet<Product> Products { get; set; }
-
-        DbSet<Charm> Charms { get; set; }
-
-        DbSet<ProductCategory> ProductCategories { get; set; }
-
-        DbSet<CharmCategory> CharmCategories { get; set; }
+        DbSet<User> Users { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 
--- src/Streetwood.Core/Domain/Implementation/StreetwoodContext.cs
+++ src/Streetwood.Core/Domain/Implementation/StreetwoodContext.cs
@@ -7,41 +7,55 @@ namespace Streetwood.Core.Domain.Implementation
 {
     internal class StreetwoodContext : DbContext, IDbContext
     {
-        public StreetwoodContext(DbContextOptions<StreetwoodContext> options)
-            :base(options)
-        {
-        }
-
-        public DbSet<User> Users { get; set; }
-
         public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Charm> Charms { get; set; }
+
+        public DbSet<CharmCategory> CharmCategories { get; set; }
+
+        public DbSet<Image> Images { get; set; }
 
         public DbSet<Order> Orders { get; set; }
 
-        public DbSet<Shippment> Shippments { get; set; }
+        public DbSet<OrderDiscount> OrderDiscounts { get; set; }
 
-        public DbSet<ProductDiscount> Discounts { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<ProductCategoryDiscount> ProductCategoryDiscounts { get; set; }
 
         public DbSet<ProductOrder> ProductOrders { get; set; }
 
         public DbSet<ProductOrderCharm> ProductOrderCharms { get; set; }
 
-        public DbSet<Image> Images { get; set; }
+        public DbSet<Shippment> Shippments { get; set; }
 
-        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
 
-        public DbSet<Charm> Charms { get; set; }
-
-        public DbSet<ProductCategory> ProductCategories { get; set; }
-
-        public DbSet<CharmCategory> CharmCategories { get; set; }
+        public StreetwoodContext(DbContextOptions<StreetwoodContext> options)
+            : base(options)
+        {
+        }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.ApplyConfiguration(new UserConfiguration());
-            builder.ApplyConfiguration(new ShippmentConfiguration());
             builder.ApplyConfiguration(new AddressConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new CharmCategoryConfiguration());
+            builder.ApplyConfiguration(new CharmCategoryConfiguration());
+            builder.ApplyConfiguration(new ImageConfiguration());
             builder.ApplyConfiguration(new OrderConfiguration());
+            builder.ApplyConfiguration(new OrderDiscountConfiguration());
+            builder.ApplyConfiguration(new ProductConfiguration());
+            builder.ApplyConfiguration(new ProductCategoryConfiguration());
+            builder.ApplyConfiguration(new ProductCategoryDiscountConfiguration());
+            builder.ApplyConfiguration(new ProductOrderConfiguration());
+            builder.ApplyConfiguration(new ProductOrderCharmConfiguration());
+            builder.ApplyConfiguration(new ShippmentConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
         }
     }
 }
