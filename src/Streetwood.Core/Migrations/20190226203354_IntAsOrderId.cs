7b2bd6d9867f604b197686c3f8550b9a0621a451
Marcin Stelmach <marcinxd4@gmail.com>
2019-02-26 21:47:11 +0100

    Added IntAsOrderId migration

---
+++ src/Streetwood.Core/Migrations/20190226203354_IntAsOrderId.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Streetwood.Core.Migrations
+{
+    public partial class IntAsOrderId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "ProductOrders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(Guid))
+                .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "ProductOrders");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "Orders",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+        }
+    }
+}
