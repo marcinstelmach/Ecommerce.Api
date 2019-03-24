7fa7a8826cea3b85c58bea12b63e28a466bb00bf
Marcin Stelmach <marcinxd4@gmail.com>
2019-03-24 19:08:44 +0100

    Added NewOrder email template

---
+++ src/Streetwood.Infrastructure/Managers/Abstract/IEmailManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Streetwood.Core.Domain.Entities;
+
+namespace Streetwood.Infrastructure.Managers.Abstract
+{
+    public interface IEmailManager
+    {
+        Task SendAsync(string receiver, string subject, string body);
+
+        Task PrepareNewOrderEmailAsync(Order order);
+
+        Task PrepareNewUserEmailAsync(User user);
+
+        Task ForgottenPasswordEmailAsync(User user);
+    }
+}
