20433b2e7b54048bc29fc2ae7f62b5036a63c7ee
Marcin Stelmah <marcinxd4@gmail.com>
2021-02-13 10:42:25 +0100

    Adding slide

---
+++ src/Streetwood.Core/Migrations/20210213091612_AddedPayments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Streetwood.Core.Migrations
+{
+    public partial class AddedPayments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Slides",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    OrderIndex = table.Column<int>(nullable: false),
+                    Text = table.Column<string>(maxLength: 512, nullable: true),
+                    TextEng = table.Column<string>(maxLength: 512, nullable: true),
+                    ImageUrl = table.Column<string>(maxLength: 256, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slides", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Slides");
+        }
+    }
+}
